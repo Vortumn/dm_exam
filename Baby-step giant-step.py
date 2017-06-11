d61d0748a78e668446ce6ea21d4e290257e09dbf
vsmaxim8 <vsmaxim8@gmail.com>
2017-06-11 15:48:39 +0300

    Utility fixes

--- Baby-step giant-step.py
+++ Baby-step giant-step.py
@@ -1,12 +1,13 @@
 import math
 
-def isSimple(n): #проверка на простоту числа
+def isPrime(n): #проверка на простоту числа
     if n % 2 == 0:
         return n == 2
     d = 3
-    while d * d <= n and n % d != 0:
-       d += 2
-    return d * d > n
+    for i in range(d, math.ceil(math.sqrt(n)) + 1, 2):
+        if (n %  d == 0):
+            return False
+    return True
 
 print("Поиск решения уравнения a^x = b (mod p)")
 
@@ -14,59 +15,34 @@ print("Поиск решения уравнения a^x = b (mod p)")
 while True:
     print('Введите простое число p')
     p = int(input())
-    if isSimple(p):
+    if isPrime(p):
         break
 
 print('Введите a')
 a = int(input())
-b = 9999**99
+b = p + 1
 
 while (b>p):
     print('Введите b < p')
     b = int(input())
 
 
-H = int(p ** (1 / 2)) + 1
+H = int(math.sqrt(p)) + 1
+print("H Is ", H)
 
 #print('H = ', H)
 
 c = (a ** H) % p
 #print('c = ', c)
-u = {}
-v = {}
-u[0] = 0
-
-for i in range(1, H + 1):
-    u[i] = ((c ** i) % p)
-u = list(u.items())
-u.sort(key=lambda item: item[1]) #магия для сортировок значений по возрастанию с сохранением их ключа
-u = dict(u)
+# kinda magic
+u = sorted([(i, (c ** i) % p) for i in range(1, H + 1)], key = lambda item: item[1])
+v = sorted([(i, b * a ** i) for i in range(H + 1)], key = lambda item: item[1])
 
 
-for k in range(H + 1):          #магия для сортировок значений по возрастанию с сохранением их ключа
-    v[k]=((b * (a ** k)) % p)
-v = list(v.items())
-v.sort(key=lambda item: item[1])
-v = dict(v)
+for i in u:
+    for j in v:
+        if i[1] == j[1]:
+            x = (i[0] * H - j[0]) % (p - 1)
+            break
 
-#print(u)
-#print(v)
-
-#x = []
-x = 0
-
-# нужно хранить индексы для совпадающих элементов(u и v)
-# хотя по сути достаточно найти первые совпадающие элементы и по одному u, v
-# x = H*u - v (mod p-1)
-
-for key in u:
-    for keys in  v:
-        if v[keys] == u[key]:
-            #x.append((H*key - keys) % (p-1))
-            x = (H*key - keys) % (p-1)
-            break #чтобы не перебирать словари до упора, выход из цикла при первом найденном x
-
-print()
-
-print(a, "^", x, " = ", b, "mod(",p, ")")
-
+print("{}^{} = {} (mod  {})".format(a, x, b, p))
